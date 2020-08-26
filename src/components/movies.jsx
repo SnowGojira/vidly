ddc7994b8971c81de7215ddeb2c803e6cc58a47a
SirSnow <hakuhal.snow@gmail.com>
2020-08-26 19:09:49 +0800

    table的雏形已经做出来了

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -6,9 +6,25 @@ class Movies extends Component {
         movies:getMovies()
      }
     render() { 
-        return ( 
-            <div>{this.state.movies.toString()}</div>
-         );
+        return <React.Fragment>
+            <hr className="m-4"></hr>
+            <div className="row mb-3" style={{fontWeight:'bold'}}>
+                <div className="col-md-4 themed-grid-col">Title</div>
+                <div className="col-md-2 themed-grid-col">Genre</div>
+                <div className="col-md-2 themed-grid-col">Stock</div>
+                <div className="col-md-2 themed-grid-col">Rate</div>
+                <div className="col-md-2 themed-grid-col"></div>
+            </div>
+            <div className="row mb-3">
+                <div className="col-md-4 themed-grid-col">Title</div>
+                <div className="col-md-2 themed-grid-col">Genre</div>
+                <div className="col-md-2 themed-grid-col">Stock</div>
+                <div className="col-md-2 themed-grid-col">Rate</div>
+                <div className="col-md-2 themed-grid-col">
+                    <button>delete</button>
+                </div>
+            </div>
+        </React.Fragment>;
     }
 }
  
