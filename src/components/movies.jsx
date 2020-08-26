d0ab168fa36d3aa701988102d75b6a428e36fd5c
SirSnow <hakuhal.snow@gmail.com>
2020-08-26 20:46:41 +0800

    数据的title做出来了

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -16,7 +16,7 @@ class Movies extends Component {
                 <div className="col-md-2 themed-grid-col"></div>
             </div>
             <div className="row mb-3">
-                <div className="col-md-4 themed-grid-col">Title</div>
+                <div className="col-md-4 themed-grid-col">{this.state.movies[0].title}</div>
                 <div className="col-md-2 themed-grid-col">Genre</div>
                 <div className="col-md-2 themed-grid-col">Stock</div>
                 <div className="col-md-2 themed-grid-col">Rate</div>
