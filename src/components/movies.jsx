153d26d0e0b7268ec8ea42f3ef31d78d34ed773c
SirSnow <hakuhal.snow@gmail.com>
2020-08-26 21:00:19 +0800

    列表出来了

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -5,6 +5,19 @@ class Movies extends Component {
     state = { 
         movies:getMovies()
      }
+    
+    movieSection(movie){
+        // console.log(movie);
+        return <div className="row mb-3" key={movie._id}>
+                <div className="col-md-4 themed-grid-col">{movie.title}</div>
+                <div className="col-md-2 themed-grid-col">{movie.genre.name}</div>
+                <div className="col-md-2 themed-grid-col">{movie.numberInStock}</div>
+                <div className="col-md-2 themed-grid-col">{movie.dailyRentalRate}</div>
+                <div className="col-md-2 themed-grid-col">
+                    <button>delete</button>
+                </div>
+            </div>
+    }
     render() { 
         return <React.Fragment>
             <hr className="m-4"></hr>
@@ -15,15 +28,10 @@ class Movies extends Component {
                 <div className="col-md-2 themed-grid-col">Rate</div>
                 <div className="col-md-2 themed-grid-col"></div>
             </div>
-            <div className="row mb-3">
-                <div className="col-md-4 themed-grid-col">{this.state.movies[0].title}</div>
-                <div className="col-md-2 themed-grid-col">Genre</div>
-                <div className="col-md-2 themed-grid-col">Stock</div>
-                <div className="col-md-2 themed-grid-col">Rate</div>
-                <div className="col-md-2 themed-grid-col">
-                    <button>delete</button>
-                </div>
-            </div>
+            
+               {this.state.movies.map(movie=>this.movieSection(movie))}
+            
+            
         </React.Fragment>;
     }
 }
