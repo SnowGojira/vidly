83266676e1f91bac269b2bb00fa26a08bca8f2f1
SirSnow <hakuhal.snow@gmail.com>
2020-09-03 17:22:49 +0800

    事件框架调通

--- src/components/pagination.jsx
+++ src/components/pagination.jsx
@@ -7,15 +7,15 @@ class Pagination extends Component {
     let pageCount = Math.ceil(total / pageSize);
     let pages = _.range(1, pageCount + 1);
 
+    if (pageCount === 1) pages = [];
+
     return (
       <ul className="pagination">
-        {pages.map((page) => {
-          return (
-            <li key={page} className="page-item">
-              <span className="page-link">{page}</span>
-            </li>
-          );
-        })}
+        {pages.map((page) => (
+          <li key={page} onClick={onPageChange} className="page-item">
+            <span className="page-link">{page}</span>
+          </li>
+        ))}
       </ul>
     );
   }
