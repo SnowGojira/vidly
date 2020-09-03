216d536d2b34d023271ce7adaf45487d0c195812
SirSnow <hakuhal.snow@gmail.com>
2020-09-03 16:54:02 +0800

    将分页显示部分逻辑放在一起，先不分割

--- src/components/pagination.jsx
+++ src/components/pagination.jsx
@@ -2,24 +2,20 @@ import React, { Component } from "react";
 import _ from "lodash";
 
 class Pagination extends Component {
-  pageSection(page) {
-    return (
-      <li key={page} className="page-item">
-        <span className="page-link">
-          {page}
-          <span className="sr-only">(current)</span>
-        </span>
-      </li>
-    );
-  }
   render() {
     let { total, pageSize, onPageChange } = this.props;
-    let pageCount = total / pageSize;
+    let pageCount = Math.ceil(total / pageSize);
     let pages = _.range(1, pageCount + 1);
 
     return (
-      <ul className="pagination pagination-sm">
-        {pages.map((page) => this.pageSection(page))}
+      <ul className="pagination">
+        {pages.map((page) => {
+          return (
+            <li key={page} className="page-item">
+              <span className="page-link">{page}</span>
+            </li>
+          );
+        })}
       </ul>
     );
   }
