f4ad6701a5875f2cd71133bbffd097910d7e3e3f
SirSnow <hakuhal.snow@gmail.com>
2020-09-11 10:58:56 +0800

    添加好了Navbar的样式

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./App.css";
 import Movies from "./components/movies";
-
+import { Route, Switch } from "react-router-dom";
+import NavBar from "./components/navBar";
 function App() {
   return (
     <main className="container">
+      <NavBar />
       <Movies />
     </main>
   );
