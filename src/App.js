2eccc6ba540dce1b49080655b9c8bad3ff5c11cc
SirSnow <hakuhal.snow@gmail.com>
2020-09-11 11:09:20 +0800

    写nav的功能

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React from "react";
 import "./App.css";
 import Movies from "./components/movies";
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, Redirect } from "react-router-dom";
 import NavBar from "./components/navBar";
+import Customers from "./components/customers";
+import Rentals from "./components/rentals";
 function App() {
   return (
     <main className="container">
       <NavBar />
-      <Movies />
+      <Switch>
+        <Route path="/movies" component={Movies} />
+        <Route path="/customers" component={Customers} />
+        <Route path="/rentals" component={Rentals} />
+        <Redirect from="/" to="/movies" />
+      </Switch>
     </main>
   );
 }
