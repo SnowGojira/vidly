f8b4e5e138feb24f50ba5e2cd1b91f628280591a
haku <hakuhal.snow@gmail.com>
2020-10-19 19:25:58 +0800

    添加了axios一个interceptor来主管错误。

--- src/services/httpService.js
+++ src/services/httpService.js
@@ -1,5 +1,13 @@
 import axios from "axios";
 
+axios.interceptors.use(null,err=>{
+  const clientError = err.response && err.response.status >=400 && err.response.status <500;
+  if(!clientError){
+    console.log("An unexcepted error happened!:"+err);
+  }
+  return Promise.reject(err);
+})
+
 export default {
   get: axios.get,
   post: axios.post,
