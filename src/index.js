c75b8435f6aeacee365cd59e41204979bc1995cf
haku <hakuhal.snow@gmail.com>
2020-10-20 13:43:07 +0800

    暂时不需要复杂的log，简化一下log

--- src/index.js
+++ src/index.js
@@ -6,9 +6,9 @@ import * as serviceWorker from "./serviceWorker";
 import "bootstrap/dist/css/bootstrap.css";
 import "font-awesome/css/font-awesome.css";
 import { BrowserRouter } from "react-router-dom";
-import logger from "./services/logService";
+// import logger from "./services/logService";
 
-logger.init();
+// logger.init();
 
 ReactDOM.render(
   <BrowserRouter>
--- src/services/logService.js
+++ src/services/logService.js
@@ -1,22 +1,19 @@
-import * as Sentry from "@sentry/react";
-import {Integrations} from "@sentry/tracing";
+// import * as Sentry from "@sentry/react";
+// import {Integrations} from "@sentry/tracing";
 
 function init(){
-    Sentry.init({
-        dsn: "https://d2a083ed43c74f69ba074faad434721c@o461699.ingest.sentry.io/5471374",
-        integrations: [
-          new Integrations.BrowserTracing(),
-        ],
-      
-        // We recommend adjusting this value in production, or using tracesSampler
-        // for finer control
-        tracesSampleRate: 1.0,
-      });
+    // Sentry.init({
+    //     dsn: "https://d2a083ed43c74f69ba074faad434721c@o461699.ingest.sentry.io/5471374",
+    //     integrations: [
+    //       new Integrations.BrowserTracing(),
+    //     ],
+    //     tracesSampleRate: 1.0,
+    //   });
 }
 
 function log(err){
-    console.log("something Happened!"+err);
-    Sentry.captureException(err);
+    console.error("something Happened!"+err);
+    //Sentry.captureException(err);
 }
 
 
